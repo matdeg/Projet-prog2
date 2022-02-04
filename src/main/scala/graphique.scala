26fa2e60f06a1406dc6dcfc9b8c4e2cf93aa8fcf
Gaspard Reghem <gaspard.reghem@gmail.com>
2022-02-04 19:29:51 +0100

    affichage msgbox inter

--- src/main/scala/graphique.scala
+++ src/main/scala/graphique.scala
@@ -13,7 +13,7 @@ class Fenetre extends JFrame {
     setLayout(null)
     setUndecorated(true)
     setContentPane(new Affichage)
-    setSize(512, 768)
+    setSize(500, 800)
     setLocation(500, 250)
     setVisible(true)
 }
@@ -21,11 +21,25 @@ class Fenetre extends JFrame {
 class Affichage extends JPanel {
 
     var devant : BufferedImage = ImageIO.read(getClass.getResource("Devant.png"));
-    
+    var derriere : BufferedImage = ImageIO.read(getClass.getResource("Derriere.png"));
+
+    def draw_button (g : Graphics): Unit = {
+        g.setColor(Color.GRAY)
+        g.fillRect(0, 500, 500, 300)
+    }
+
+    def draw_msgbox (g : Graphics) : Unit = {
+        g.setColor(Color.BLACK)
+        g.fillRect(10, 400, 480, 100)
+        g.setColor(Color.WHITE)
+        g.fillRect(20, 410, 460, 80)
+    }
     override def paintComponent (g : Graphics) : Unit = {
         super.paintComponent (g)
         g.setColor(Color.red)
-        g.drawImage(devant, 0, 0, null)
-        println(devant)
+        g.drawImage(devant, 300, 50, null)
+        g.drawImage(derriere, 50, 250, null)
+        draw_button (g)
+        draw_msgbox (g)
     }
 }
