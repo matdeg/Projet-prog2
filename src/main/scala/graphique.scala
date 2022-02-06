f01768945d262083fdfdc2da5b8836a6db1d4cee
Gaspard Reghem <gaspard.reghem@gmail.com>
2022-02-06 13:44:05 +0100

    layout fonctionnel

--- src/main/scala/graphique.scala
+++ src/main/scala/graphique.scala
@@ -1,5 +1,7 @@
 import java.awt.{BorderLayout, Dimension, Graphics, Color, Font, Graphics2D, Image}
 import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JButton
 import javax.swing.JScrollPane
 import javax.swing.JTextArea
 import javax.swing.JPanel
@@ -7,39 +9,62 @@ import javax.imageio.ImageIO
 import java.io.File
 import java.io.IOException
 import java.awt.image.BufferedImage
+import java.awt.GridLayout
+import javax.swing.BoxLayout
+import java.text.AttributedString
 
-class Fenetre extends JFrame {
-
-    setLayout(null)
-    setUndecorated(true)
-    setContentPane(new Affichage)
-    setSize(500, 800)
-    setLocation(500, 250)
-    setVisible(true)
-}
-
-class Affichage extends JPanel {
+class AffichageBataille extends JPanel {
 
     var devant : BufferedImage = ImageIO.read(getClass.getResource("Devant.png"));
     var derriere : BufferedImage = ImageIO.read(getClass.getResource("Derriere.png"));
 
-    def draw_button (g : Graphics): Unit = {
-        g.setColor(Color.GRAY)
-        g.fillRect(0, 500, 500, 300)
-    }
-
-    def draw_msgbox (g : Graphics) : Unit = {
-        g.setColor(Color.BLACK)
-        g.fillRect(10, 400, 480, 100)
-        g.setColor(Color.WHITE)
-        g.fillRect(20, 410, 460, 80)
-    }
     override def paintComponent (g : Graphics) : Unit = {
         super.paintComponent (g)
-        g.setColor(Color.red)
         g.drawImage(devant, 300, 50, null)
         g.drawImage(derriere, 50, 250, null)
-        draw_button (g)
-        draw_msgbox (g)
     }
+}
+
+
+class Fenetre extends JFrame {
+
+    this.setTitle("Best Game Ever")
+    this.setSize(500, 800)
+    this.setLocation(500, 250)
+
+    var bataille = new AffichageBataille ()
+    
+    var msgbox = new JPanel ()
+    msgbox.setBounds(0, 500, 500, 100)
+    var texte = new JLabel ("azerty")
+    msgbox.add(texte)
+    
+
+    var interaction = new JPanel ()
+    interaction.setLayout(new GridLayout(2,2))
+
+    var boutonhg = new JButton ("Haut Gauche")
+    var boutonhd = new JButton ("Haut Droit")
+    var boutonbg = new JButton ("Bas Gauche")
+    var boutonbd = new JButton ("Bas Droit")
+
+    interaction.add(boutonhg)
+    interaction.add(boutonhd)
+    interaction.add(boutonbg)
+    interaction.add(boutonbd)
+
+    var bas_fenetre = new JPanel
+    bas_fenetre.setLayout(new GridLayout(2,1))
+    bas_fenetre.add(msgbox)
+    bas_fenetre.add(interaction)
+
+    var total = new JPanel
+    total.setLayout(new GridLayout(2,1))
+    total.add(bataille)
+    total.add(bas_fenetre)
+
+    this.setContentPane(total)
+
+    setVisible(true)
+
 }
