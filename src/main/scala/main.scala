1d74a4903e401ff4a8db14698a25039e5148060c
Mathis Degryse <degryse.mathis@gmail.com>
2022-02-07 16:39:03 +0100

    Changement de pokémon dans le menu

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,10 +4,6 @@ import java.util.Random
 
 object MyProgram {
     def main(args: Array[String]): Unit = {
-        var ecran = new Fenetre
-        var p = new Pokemon_test1("pikachu")
-        ecran.msgbox.print_msg("test")
-        ecran.bataille.print_pok_perso(p)
-        ecran.bataille.print_pok_op(p)
+        test_mathis.start
     }
 }
