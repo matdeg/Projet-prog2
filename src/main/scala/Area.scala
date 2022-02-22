830cc530b4dae3f24d0da39cfdc3981c052d2c0c
Mathis Degryse <degryse.mathis@gmail.com>
2022-02-22 14:18:02 +0100

    rien

--- src/main/scala/Area.scala
+++ src/main/scala/Area.scala
@@ -12,6 +12,7 @@ object O extends Direction {override def newx(x : Int) = x - 1;
                             override def newy(y : Int) = y}
 
 abstract class Area {
+    var img = ""
     var w : Int = 10
     var h : Int = 10
     var tab : Array[Array[Seenable]] = Array.fill(h,w){ Empty_seenable }
--- src/main/scala/test_mathis.scala
+++ src/main/scala/test_mathis.scala
@@ -6,10 +6,5 @@ object test_mathis {
             for (i <- 0 to 10) Player.bag(i) = 2
             Atrium.add_character(Player,5,5)
             Atrium.add_character(Example_opponent1,7,7)
-            Player.move(N)
-            Player.move(N)
-            Example_opponent1.move(O)
-            Player.move(E)
-            Player.interact()
 }
     
